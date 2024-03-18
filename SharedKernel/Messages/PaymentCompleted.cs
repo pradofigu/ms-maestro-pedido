1ea5b6d965530cd6841e0fb56d619938408cf7ea
Fábio Badaró <fbadaro2@gmail.com>
2024-03-18 18:14:36 -0300

    feat: adjust contract to payments service

--- SharedKernel/Messages/PaymentCompleted.cs
+++ SharedKernel/Messages/PaymentCompleted.cs
@@ -4,15 +4,15 @@ namespace SharedKernel.Messages
 {
     public interface IPaymentCompleted
     {
-        public Guid OrderId { get; set; }
-
         public Guid PaymentId { get; set; }
+        public Guid CorrelationId { get; set; }
+        public string Status { get; set; }
     }
 
     public class PaymentCompleted : IPaymentCompleted
     {
-        public Guid OrderId { get; set; }
-
         public Guid PaymentId { get; set; }
+        public Guid CorrelationId { get; set; }
+        public string Status { get; set; }
     }
 }
